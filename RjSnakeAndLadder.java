f4493dfa3a66e00007c03b1a0ccb342a02007b08
Siva Prasad . K <siva>
2021-12-12 11:33:37 +0000

    main file is modified by siva prasad

--- RjSnakeAndLadder.java
+++ RjSnakeAndLadder.java
@@ -58,7 +58,8 @@ public class RjSnakeAndLadder {
         while(noOfPlayers > 1){
             for(int i = 0; i < noOfPlayers; i++){
                 int diceValue = d.rollADice();
-                int currPosition = playersPositions.get(players.get(i));
+                String playerName = players.get(i);
+                int currPosition = playersPositions.get(playerName);
                 if(currPosition+diceValue <= 100){
                     int nextPosition;
                     if(headsAndTails.containsKey(currPosition+diceValue)){
@@ -70,15 +71,29 @@ public class RjSnakeAndLadder {
                     else{
                         nextPosition = currPosition+diceValue;
                     }
-                    System.out.println(players.get(i)+" rolled a "+diceValue+" and moved from "+currPosition+" to "+nextPosition);
-                    playersPositions.put(players.get(i), nextPosition);
+                    int prevPosition = nextPosition;
+                    while(headsAndTails.containsKey(nextPosition) || startsAndEnds.containsKey(nextPosition)){
+                        if(headsAndTails.containsKey(nextPosition)){
+                            nextPosition = headsAndTails.get(nextPosition);
+                        }
+                        else if(startsAndEnds.containsKey(nextPosition)){
+                            nextPosition = startsAndEnds.get(nextPosition);
+                        }
+                        if(prevPosition == nextPosition){
+//                            System.out.println("loop");
+                            nextPosition = currPosition;
+                            break;
+                        }
+                    }
+                    System.out.println(playerName+" rolled a "+diceValue+" and moved from "+currPosition+" to "+nextPosition);
+                    playersPositions.put(playerName, nextPosition);
                 }
                 else{
-                    System.out.println(players.get(i)+" rolled a "+diceValue+" and moved from "+currPosition+" to "+currPosition);
+                    System.out.println(playerName+" rolled a "+diceValue+" and moved from "+currPosition+" to "+currPosition);
                 }
-                if(playersPositions.get(players.get(i)) == 100){
-                    winners.add(players.get(i));
-                    players.remove(players.get(i));
+                if(playersPositions.get(playerName) == 100){
+                    winners.add(playerName);
+                    players.remove(playerName);
                     noOfPlayers--;
                 }
             }
@@ -91,3 +106,189 @@ public class RjSnakeAndLadder {
         }
     }
 }
+
+
+
+//Enter number of Snakes : 3
+//Enter head 1: 64
+//Enter tail 1: 32
+//Enter head 2: 84
+//Enter tail 2: 64
+//Enter head 3: 55
+//Enter tail 3: 34
+//Enter number of Ladders : 2
+//Enter start 1: 32
+//Enter end 1: 84
+//Enter start 2: 14
+//Enter end 2: 55
+//Enter number of Players : 5
+//Enter Player 1: siva
+//Enter Player 2: rishika
+//Enter Player 3: raju
+//Enter Player 4: ram
+//Enter Player 5: ravi
+//Enter minimum value of Dice : 1
+//Enter maximum value of Dice : 6
+//siva rolled a 4 and moved from 0 to 4
+//rishika rolled a 3 and moved from 0 to 3
+//raju rolled a 4 and moved from 0 to 4
+//ram rolled a 2 and moved from 0 to 2
+//ravi rolled a 6 and moved from 0 to 6
+//siva rolled a 6 and moved from 4 to 10
+//rishika rolled a 1 and moved from 3 to 4
+//raju rolled a 2 and moved from 4 to 6
+//ram rolled a 3 and moved from 2 to 5
+//ravi rolled a 6 and moved from 6 to 12
+//siva rolled a 6 and moved from 10 to 16
+//rishika rolled a 1 and moved from 4 to 5
+//raju rolled a 1 and moved from 6 to 7
+//ram rolled a 6 and moved from 5 to 11
+//ravi rolled a 2 and moved from 12 to 34
+//siva rolled a 4 and moved from 16 to 20
+//rishika rolled a 3 and moved from 5 to 8
+//raju rolled a 1 and moved from 7 to 8
+//ram rolled a 5 and moved from 11 to 16
+//ravi rolled a 1 and moved from 34 to 35
+//siva rolled a 5 and moved from 20 to 25
+//rishika rolled a 1 and moved from 8 to 9
+//raju rolled a 3 and moved from 8 to 11
+//ram rolled a 6 and moved from 16 to 22
+//ravi rolled a 1 and moved from 35 to 36
+//siva rolled a 6 and moved from 25 to 31
+//rishika rolled a 3 and moved from 9 to 12
+//raju rolled a 6 and moved from 11 to 17
+//ram rolled a 5 and moved from 22 to 27
+//ravi rolled a 4 and moved from 36 to 40
+//siva rolled a 4 and moved from 31 to 35
+//rishika rolled a 1 and moved from 12 to 13
+//raju rolled a 2 and moved from 17 to 19
+//ram rolled a 6 and moved from 27 to 33
+//ravi rolled a 3 and moved from 40 to 43
+//siva rolled a 1 and moved from 35 to 36
+//rishika rolled a 4 and moved from 13 to 17
+//raju rolled a 4 and moved from 19 to 23
+//ram rolled a 4 and moved from 33 to 37
+//ravi rolled a 2 and moved from 43 to 45
+//siva rolled a 6 and moved from 36 to 42
+//rishika rolled a 1 and moved from 17 to 18
+//raju rolled a 2 and moved from 23 to 25
+//ram rolled a 3 and moved from 37 to 40
+//ravi rolled a 4 and moved from 45 to 49
+//siva rolled a 3 and moved from 42 to 45
+//rishika rolled a 5 and moved from 18 to 23
+//raju rolled a 5 and moved from 25 to 30
+//ram rolled a 3 and moved from 40 to 43
+//ravi rolled a 4 and moved from 49 to 53
+//siva rolled a 6 and moved from 45 to 51
+//rishika rolled a 1 and moved from 23 to 24
+//raju rolled a 1 and moved from 30 to 31
+//ram rolled a 1 and moved from 43 to 44
+//ravi rolled a 4 and moved from 53 to 57
+//siva rolled a 6 and moved from 51 to 57
+//rishika rolled a 2 and moved from 24 to 26
+//raju rolled a 4 and moved from 31 to 35
+//ram rolled a 5 and moved from 44 to 49
+//ravi rolled a 5 and moved from 57 to 62
+//siva rolled a 5 and moved from 57 to 62
+//rishika rolled a 5 and moved from 26 to 31
+//raju rolled a 5 and moved from 35 to 40
+//ram rolled a 3 and moved from 49 to 52
+//ravi rolled a 4 and moved from 62 to 66
+//siva rolled a 4 and moved from 62 to 66
+//rishika rolled a 1 and moved from 31 to 31
+//raju rolled a 5 and moved from 40 to 45
+//ram rolled a 3 and moved from 52 to 34
+//ravi rolled a 5 and moved from 66 to 71
+//siva rolled a 4 and moved from 66 to 70
+//rishika rolled a 2 and moved from 31 to 33
+//raju rolled a 4 and moved from 45 to 49
+//ram rolled a 5 and moved from 34 to 39
+//ravi rolled a 2 and moved from 71 to 73
+//siva rolled a 5 and moved from 70 to 75
+//rishika rolled a 4 and moved from 33 to 37
+//raju rolled a 2 and moved from 49 to 51
+//ram rolled a 2 and moved from 39 to 41
+//ravi rolled a 2 and moved from 73 to 75
+//siva rolled a 6 and moved from 75 to 81
+//rishika rolled a 6 and moved from 37 to 43
+//raju rolled a 3 and moved from 51 to 54
+//ram rolled a 1 and moved from 41 to 42
+//ravi rolled a 4 and moved from 75 to 79
+//siva rolled a 1 and moved from 81 to 82
+//rishika rolled a 2 and moved from 43 to 45
+//raju rolled a 2 and moved from 54 to 56
+//ram rolled a 2 and moved from 42 to 44
+//ravi rolled a 1 and moved from 79 to 80
+//siva rolled a 1 and moved from 82 to 83
+//rishika rolled a 1 and moved from 45 to 46
+//raju rolled a 3 and moved from 56 to 59
+//ram rolled a 6 and moved from 44 to 50
+//ravi rolled a 2 and moved from 80 to 82
+//siva rolled a 1 and moved from 83 to 83
+//rishika rolled a 3 and moved from 46 to 49
+//raju rolled a 2 and moved from 59 to 61
+//ram rolled a 3 and moved from 50 to 53
+//ravi rolled a 5 and moved from 82 to 87
+//siva rolled a 5 and moved from 83 to 88
+//rishika rolled a 6 and moved from 49 to 34
+//raju rolled a 1 and moved from 61 to 62
+//ram rolled a 4 and moved from 53 to 57
+//ravi rolled a 3 and moved from 87 to 90
+//siva rolled a 3 and moved from 88 to 91
+//rishika rolled a 3 and moved from 34 to 37
+//raju rolled a 4 and moved from 62 to 66
+//ram rolled a 5 and moved from 57 to 62
+//ravi rolled a 3 and moved from 90 to 93
+//siva rolled a 3 and moved from 91 to 94
+//rishika rolled a 1 and moved from 37 to 38
+//raju rolled a 4 and moved from 66 to 70
+//ram rolled a 3 and moved from 62 to 65
+//ravi rolled a 5 and moved from 93 to 98
+//siva rolled a 2 and moved from 94 to 96
+//rishika rolled a 1 and moved from 38 to 39
+//raju rolled a 4 and moved from 70 to 74
+//ram rolled a 2 and moved from 65 to 67
+//ravi rolled a 4 and moved from 98 to 98
+//siva rolled a 4 and moved from 96 to 100
+//raju rolled a 2 and moved from 74 to 76
+//ram rolled a 2 and moved from 67 to 69
+//ravi rolled a 3 and moved from 98 to 98
+//rishika rolled a 2 and moved from 39 to 41
+//raju rolled a 2 and moved from 76 to 78
+//ram rolled a 2 and moved from 69 to 71
+//ravi rolled a 1 and moved from 98 to 99
+//rishika rolled a 6 and moved from 41 to 47
+//raju rolled a 3 and moved from 78 to 81
+//ram rolled a 5 and moved from 71 to 76
+//ravi rolled a 1 and moved from 99 to 100
+//rishika rolled a 6 and moved from 47 to 53
+//raju rolled a 3 and moved from 81 to 81
+//ram rolled a 6 and moved from 76 to 82
+//rishika rolled a 3 and moved from 53 to 56
+//raju rolled a 2 and moved from 81 to 83
+//ram rolled a 4 and moved from 82 to 86
+//rishika rolled a 2 and moved from 56 to 58
+//raju rolled a 4 and moved from 83 to 87
+//ram rolled a 3 and moved from 86 to 89
+//rishika rolled a 6 and moved from 58 to 58
+//raju rolled a 2 and moved from 87 to 89
+//ram rolled a 1 and moved from 89 to 90
+//rishika rolled a 1 and moved from 58 to 59
+//raju rolled a 5 and moved from 89 to 94
+//ram rolled a 2 and moved from 90 to 92
+//rishika rolled a 5 and moved from 59 to 59
+//raju rolled a 6 and moved from 94 to 100
+//rishika rolled a 2 and moved from 59 to 61
+//ram rolled a 1 and moved from 92 to 93
+//rishika rolled a 4 and moved from 61 to 65
+//ram rolled a 2 and moved from 93 to 95
+//rishika rolled a 2 and moved from 65 to 67
+//ram rolled a 4 and moved from 95 to 99
+//rishika rolled a 4 and moved from 67 to 71
+//ram rolled a 1 and moved from 99 to 100
+//*********************************Game is Completed*********************************
+//                                      Winners
+//                               siva Secured Position 1
+//                               ravi Secured Position 2
+//                               raju Secured Position 3
+//                               ram Secured Position 4
